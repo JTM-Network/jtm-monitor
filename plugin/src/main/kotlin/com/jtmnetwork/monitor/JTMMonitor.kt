3bf9068af255ad42ad8fc726099f9c5f60a42920
Joe Matthews <joetymatthews@gmail.com>
2022-04-26 15:30:27 +0100

    Initial project files:
    - Gradle will be used as a dependency management
    - Use kotlin as programming language

---
+++ plugin/src/main/kotlin/com/jtmnetwork/monitor/JTMMonitor.kt
@@ -0,0 +1,16 @@
+package com.jtmnetwork.monitor
+
+import com.jtm.framework.Framework
+
+class JTMMonitor: Framework(false) {
+
+    override fun setup() {}
+
+    override fun init() {}
+
+    override fun enable() {}
+
+    override fun disable() {}
+
+
+}
