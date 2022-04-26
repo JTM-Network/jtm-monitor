8161d207341fe100c44e905617a6f364dc97d396
Joe Matthews <joetymatthews@gmail.com>
2022-04-26 15:35:26 +0100

    new: add module to dependency injection.

--- plugin/src/main/kotlin/com/jtmnetwork/monitor/JTMMonitor.kt
+++ plugin/src/main/kotlin/com/jtmnetwork/monitor/JTMMonitor.kt
@@ -1,10 +1,16 @@
 package com.jtmnetwork.monitor
 
+import com.google.inject.Injector
 import com.jtm.framework.Framework
+import com.jtmnetwork.monitor.entrypoint.module.MonitorModule
 
 class JTMMonitor: Framework(false) {
 
-    override fun setup() {}
+    private lateinit var injector: Injector
+
+    override fun setup() {
+        injector = injector(listOf(MonitorModule()))
+    }
 
     override fun init() {}
 
---
+++ plugin/src/main/kotlin/com/jtmnetwork/monitor/entrypoint/module/MonitorModule.kt
@@ -0,0 +1,9 @@
+package com.jtmnetwork.monitor.entrypoint.module
+
+import com.google.inject.AbstractModule
+
+class MonitorModule: AbstractModule() {
+    override fun configure() {
+
+    }
+}
