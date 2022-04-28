d07f340bab9b37d9074466c2491a41725b409c29
Joe Matthews <joetymatthews@gmail.com>
2022-04-28 19:06:35 +0100

    Remove JTMMonitorTestSuite.kt

--- plugin/src/test/kotlin/com/jtmnetwork/monitor/JTMMonitorTestSuite.kt
+++
@@ -1,8 +0,0 @@
-package com.jtmnetwork.monitor
-
-import org.junit.runner.RunWith
-import org.junit.runners.Suite
-
-//@RunWith(Suite::class)
-//@Suite.SuiteClasses(value = [])
-class JTMMonitorTestSuite
