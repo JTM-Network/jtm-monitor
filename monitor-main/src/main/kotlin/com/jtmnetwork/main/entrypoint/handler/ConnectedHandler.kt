cc2d857d0517daa1e2da9a83e1bde93c685df18c
Joe Matthews <joetymatthews@gmail.com>
2022-05-03 19:20:00 +0100

    refactor: update names of project.

--- monitor-main/src/main/kotlin/com/jtmnetwork/main/entrypoint/handler/ConnectedHandler.kt
+++ monitor-main/src/main/kotlin/com/jtmnetwork/main/entrypoint/handler/ConnectedHandler.kt
@@ -9,11 +9,20 @@ import com.jtmnetwork.main.entrypoint.configuration.ServerConfiguration
 import okhttp3.WebSocket
 import org.slf4j.LoggerFactory
 
+/**
+ * Handles the initial handshake between server and client.
+ */
 class ConnectedHandler @Inject constructor(private val configuration: ServerConfiguration): EventHandlerImpl() {
 
-    private val gson = GsonBuilder().create()
     private val logger = LoggerFactory.getLogger(ConnectedHandler::class.java)
+    private val gson = GsonBuilder().create()
 
+    /**
+     * Using the server id sent by the server, persist it in the server configuration
+     *
+     * @param socket        the web socket
+     * @param event         the event being sent.
+     */
     override fun onEvent(socket: WebSocket, event: Event) {
         val info = gson.fromJson(event.value, ServerInfo::class.java)
         configuration.setServerId(info.id)
