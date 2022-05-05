a98ed29039459e6ff6879a612827f7b9e134f14b
Joe Matthews <joetymatthews@gmail.com>
2022-05-05 14:12:58 +0100

    refactor: version 0.19

--- monitor-main/src/main/kotlin/com/jtmnetwork/monitor/entrypoint/socket/MonitorConnection.kt
+++ monitor-main/src/main/kotlin/com/jtmnetwork/monitor/entrypoint/socket/MonitorConnection.kt
@@ -13,7 +13,6 @@ import okhttp3.WebSocket
 import org.slf4j.LoggerFactory
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
-import java.util.concurrent.ThreadPoolExecutor
 
 @Singleton
 class MonitorConnection @Inject constructor(private val framework: Framework, private val configuration: ServerConfiguration, private val dispatcher: EventDispatcher) {
