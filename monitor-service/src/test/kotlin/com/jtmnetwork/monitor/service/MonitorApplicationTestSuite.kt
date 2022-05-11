d0aca047ff2c2aa40772cf78db6890d3ee7b8af2
Joe Matthews <joetymatthews@gmail.com>
2022-05-11 13:10:17 +0100

    refactor: update test suites

--- monitor-service/src/test/kotlin/com/jtmnetwork/monitor/service/MonitorApplicationTestSuite.kt
+++ monitor-service/src/test/kotlin/com/jtmnetwork/monitor/service/MonitorApplicationTestSuite.kt
@@ -1,16 +1,25 @@
 package com.jtmnetwork.monitor.service
 
+import com.jtmnetwork.monitor.service.data.service.LogServiceTest
 import com.jtmnetwork.monitor.service.data.service.ServerServiceTest
 import com.jtmnetwork.monitor.service.data.service.SessionServiceTest
+import com.jtmnetwork.monitor.service.entrypoint.controller.ServerControllerTest
 import com.jtmnetwork.monitor.service.entrypoint.controller.SessionControllerTest
+import com.jtmnetwork.monitor.service.entrypoint.handler.ConnectedHandlerTest
+import com.jtmnetwork.monitor.service.entrypoint.handler.log.IncomingLogHandlerTest
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
 @RunWith(Suite::class)
 @Suite.SuiteClasses(value = [
+    LogServiceTest::class,
     ServerServiceTest::class,
     SessionServiceTest::class,
 
     SessionControllerTest::class,
+    ServerControllerTest::class,
+
+    IncomingLogHandlerTest::class,
+    ConnectedHandlerTest::class
 ])
 class MonitorApplicationTestSuite
