463d6ee3301af2b9eeb6816bbd931faebad5d35a
Joe Matthews <joetymatthews@gmail.com>
2023-02-01 18:21:53 +0000

    new: list plugins on connect handling.

--- monitor-main/src/main/kotlin/com/jtmnetwork/monitor/entrypoint/handler/ConnectedHandler.kt
+++ monitor-main/src/main/kotlin/com/jtmnetwork/monitor/entrypoint/handler/ConnectedHandler.kt
@@ -1,24 +1,24 @@
 package com.jtmnetwork.monitor.entrypoint.handler
 
-import com.google.gson.GsonBuilder
 import com.google.inject.Inject
+import com.jtmnetwork.monitor.core.domain.dto.PluginDTO
 import com.jtmnetwork.monitor.core.domain.entity.ServerInfo
 import com.jtmnetwork.monitor.core.domain.model.Event
-import com.jtmnetwork.monitor.core.domain.model.ServerLog
+import com.jtmnetwork.monitor.core.domain.model.Plugin
 import com.jtmnetwork.monitor.core.usecase.handler.EventHandlerImpl
 import com.jtmnetwork.monitor.core.usecase.log.LogReporter
 import com.jtmnetwork.monitor.entrypoint.configuration.ServerConfiguration
 import com.jtmnetwork.monitor.entrypoint.socket.MonitorConnection
 import okhttp3.WebSocket
 import org.slf4j.LoggerFactory
+import java.util.*
 
 /**
  * Handles the initial handshake between server and client.
  */
-class ConnectedHandler @Inject constructor(private val connection: MonitorConnection, private val logReporter: LogReporter, private val configuration: ServerConfiguration): EventHandlerImpl() {
+class ConnectedHandler @Inject constructor(private val connection: MonitorConnection, private val logReporter: LogReporter, private val configuration: ServerConfiguration): EventHandlerImpl("connected_event") {
 
     private val logger = LoggerFactory.getLogger(ConnectedHandler::class.java)
-    private val gson = GsonBuilder().create()
 
     /**
      * Using the server id sent by the server, persist it in the server configuration
@@ -27,11 +27,20 @@ class ConnectedHandler @Inject constructor(private val connection: MonitorConnec
      * @param event             the event being sent.
      */
     override fun onEvent(socket: WebSocket, event: Event) {
-        val info = gson.fromJson(event.value, ServerInfo::class.java)
+        val info = getGson().fromJson(event.value, ServerInfo::class.java)
         configuration.setServerId(info.id)
         logger.info("Successfully connected to the server.")
-        val logs = logReporter.getLogs()
-        connection.sendEvent("incoming_log_event", ServerLog(logs))
-        logReporter.init()
+//        val logs = logReporter.getLogs()
+//        sendEvent(socket,"incoming_log_event", ServerLog(logs))
+        sendPlugins(socket)
+
+//        logReporter.init()
+    }
+
+    private fun sendPlugins(socket: WebSocket) {
+        val list = connection.framework.server.pluginManager.plugins.toList().stream().map { pl -> Plugin(pl.name, pl.description.version, pl.isEnabled) }.toList()
+        val map = list.associateBy { it.name }
+        sendEvent(socket,"update_plugins_event", PluginDTO(UUID.fromString(configuration.getServerId()), map))
+        logger.info("Sent plugin update.")
     }
 }
