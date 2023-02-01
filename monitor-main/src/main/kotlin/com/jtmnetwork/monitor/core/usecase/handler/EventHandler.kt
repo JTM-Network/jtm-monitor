ffa18784999b796e8be64ef4ee765e96d005b6d3
Joe Matthews <joetymatthews@gmail.com>
2023-02-01 18:15:01 +0000

    refactor: add new methods to client event handler.

--- monitor-main/src/main/kotlin/com/jtmnetwork/monitor/core/usecase/handler/EventHandler.kt
+++ monitor-main/src/main/kotlin/com/jtmnetwork/monitor/core/usecase/handler/EventHandler.kt
@@ -1,11 +1,18 @@
 package com.jtmnetwork.monitor.core.usecase.handler
 
+import com.google.gson.Gson
 import com.jtmnetwork.monitor.core.domain.model.Event
 import okhttp3.WebSocket
 
 interface EventHandler {
 
+    fun getName(): String
+
+    fun getGson(): Gson
+
     fun onEvent(socket: WebSocket, event: Event)
 
     fun sendEvent(socket: WebSocket, name: String, value: Any)
+
+    fun sendEvent(socket: WebSocket, value: Any)
 }
--- monitor-main/src/main/kotlin/com/jtmnetwork/monitor/core/usecase/handler/EventHandlerImpl.kt
+++ monitor-main/src/main/kotlin/com/jtmnetwork/monitor/core/usecase/handler/EventHandlerImpl.kt
@@ -1,14 +1,29 @@
 package com.jtmnetwork.monitor.core.usecase.handler
 
+import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.jtmnetwork.monitor.core.domain.model.Event
 import okhttp3.WebSocket
 
-abstract class EventHandlerImpl: EventHandler {
+abstract class EventHandlerImpl(private val name: String): EventHandler {
 
     private val gson = GsonBuilder().serializeNulls().create()
 
+    override fun getName(): String {
+        return this.name
+    }
+
+    override fun getGson(): Gson {
+        return this.gson
+    }
+
     override fun sendEvent(socket: WebSocket, name: String, value: Any) {
+        val event = Event(name, gson.toJson(value))
+        val json = gson.toJson(event)
+        socket.send(json)
+    }
+
+    override fun sendEvent(socket: WebSocket, value: Any) {
         val event = Event(name, gson.toJson(value))
         val json = gson.toJson(event)
         socket.send(json)
