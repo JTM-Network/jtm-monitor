e4aaea0e9df6f449cb45e32c2c9c5a05339e3abb
Joe Matthews <joetymatthews@gmail.com>
2023-02-02 16:42:37 +0000

    refactor: initial start to install a plugin to a server.

--- monitor-service/src/main/kotlin/com/jtmnetwork/monitor/service/core/usecase/spigot/ResourceInstallOperation.kt
+++ monitor-service/src/main/kotlin/com/jtmnetwork/monitor/service/core/usecase/spigot/ResourceInstallOperation.kt
@@ -1,6 +1,7 @@
 package com.jtmnetwork.monitor.service.core.usecase.spigot
 
 import com.google.gson.GsonBuilder
+import com.jtmnetwork.monitor.service.core.domain.dto.InstallRequestDTO
 import com.jtmnetwork.monitor.service.core.domain.entity.StoredPlugin
 import com.jtmnetwork.monitor.service.core.domain.model.OperationEvent
 import com.jtmnetwork.monitor.service.core.domain.model.spigot.SpigotInstallRequest
@@ -11,28 +12,41 @@ import com.jtmnetwork.monitor.service.data.service.plugin.SessionService
 import okhttp3.OkHttpClient
 import okhttp3.Request
 
-class ResourceInstallOperation(private val sessionService: SessionService, private val pluginService: StoredPluginService, private val installRequest: SpigotInstallRequest): Operation {
+class ResourceInstallOperation(private val sessionService: SessionService, private val pluginService: StoredPluginService, private val installRequest: SpigotInstallRequest, val client: OkHttpClient = OkHttpClient().newBuilder().build()): Operation {
 
     private val gson = GsonBuilder().setPrettyPrinting().create()
-    private val client = OkHttpClient().newBuilder().build()
 
     override fun execute(executor: OperationExecutor) {
+        // download the file if not found in stored plugins
+        val localPlugin = findPluginLocally(installRequest.name, installRequest.version) ?: downloadSpigotPlugin(executor, installRequest.pluginId, installRequest.name, installRequest.version)
+        if (localPlugin == null) {
+            executor.sendMessage(OperationEvent(name = "Failed to find plugin", value = "Failed to find plugin"))
+            return
+        }
+        // find the server session to install the server to
 
+        // send the link to download it from this server
+        // create a progress sink for the operation.
+        // add a complete event handler
     }
 
     private fun findPluginLocally(name: String, version: String): StoredPlugin? {
         return pluginService.getPlugin(name, version).block()
     }
 
-    private fun downloadSpigotPlugin(executor: OperationExecutor, pluginId: String): StoredPlugin? {
+    private fun downloadSpigotPlugin(executor: OperationExecutor, pluginId: String, name: String, version: String): StoredPlugin? {
         val req = Request.Builder().url("https://api.spiget.org/v2/resources/${pluginId}/download").build()
         val res = client.newCall(req).execute()
         if (!res.isSuccessful) executor.sendMessage(OperationEvent(name = "Error: ${res.code()}", value = res.message()))
+        val body = res.body() ?: return null
+        val plugin = pluginService.insertPlugin(name, version, body.bytes()).block() ?: return null
+        sendInstallRequest(installRequest, plugin)
         return null
     }
 
-    private fun sendInstallRequest(request: SpigotInstallRequest) {
-        val session = sessionService.findById(request.server).block()
+    private fun sendInstallRequest(request: SpigotInstallRequest, plugin: StoredPlugin) {
+        val session = sessionService.findById(request.server).block() ?: return
+        session.sendEvent("install_request_event", InstallRequestDTO("http://local.jtm-network.com/download/${request.server}/${plugin.id.toString()}"))
     }
 
     private fun sendInstallRequestEvent() {}
